8faa4bd6e6ff93d63c30b8ba44f0730c1030a78f
Fataev Farhod <fataev_f@mail.ru>
2020-10-22 23:02:28 +0300

    Аннотация @RequestParam - получаем параметры.

--- src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ru.alishev.springcourse.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,16 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")   // теперь полный адрес до hello или goodbye изменился localhost:8080/first/goodbye
 public class FirstController {
 
+    /*	после аннотации мы указываем название того ключа который мы ожидаем в URL (@RequestParam ("name")).
+    Spring сам возьмет этот параметр из URL'а, и положит его в параметр  (String name)*/
     @GetMapping("/hello")
-    /*В данном примере используем объект request который содержит все сведения об HTTP запросе
-    * и из него мы берем только параметры. */
-    /*Что бы передать параметры запрос должен выглядеть так :
-    *                                           localhost:8080/first/hello?name=Tom&surname=Holland
-    * посел знака ? указывается первый параметр, а второй после &*/
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        System.out.println("Hello, " + name + " " + surname); //выводим параметры в консоль
+    public String helloPage(@RequestParam (value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname){
+        System.out.println("Hello, " + name + " " + surname); // получаем готовые параметры и используем их
 
         return "first/hello";
     }
