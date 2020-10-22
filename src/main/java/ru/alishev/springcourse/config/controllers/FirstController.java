2ef0524f238440f44143cc43244a6ae539a16685
Fataev Farhod <fataev_f@mail.ru>
2020-10-22 16:11:35 +0300

    @RequestMapping

--- src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
@@ -2,13 +2,15 @@ package ru.alishev.springcourse.config.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first")   // теперь полный адрес до hello или goodbye изменился localhost:8080/first/goodbye
 public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(){
-        return "first/hello";
+        return "first/hello";       // и ссылки описанные в html файле работать не будут
     }
 
     @GetMapping("/goodbye")
