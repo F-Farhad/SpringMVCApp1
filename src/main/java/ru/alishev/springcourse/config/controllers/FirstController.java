af8998c192ace1157348961772ed15b87093d51e
Fataev Farhod <fataev_f@mail.ru>
2020-10-22 21:47:18 +0300

    HttpServletRequest - получаем параметры.

--- src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
@@ -4,13 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/first")   // теперь полный адрес до hello или goodbye изменился localhost:8080/first/goodbye
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
-        return "first/hello";       // и ссылки описанные в html файле работать не будут
+    /*В данном примере используем объект request который содержит все сведения об HTTP запросе
+    * и из него мы берем только параметры. */
+    /*Что бы передать параметры запрос должен выглядеть так :
+    *                                           localhost:8080/first/hello?name=Tom&surname=Holland
+    * посел знака ? указывается первый параметр, а второй после &*/
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello, " + name + " " + surname); //выводим параметры в консоль
+
+        return "first/hello";
     }
 
     @GetMapping("/goodbye")
